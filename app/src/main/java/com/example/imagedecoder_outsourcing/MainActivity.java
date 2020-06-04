699aa28fe3ca47901f20790d96be487f7f9be757
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2020-06-05 01:26:39 +0900

    [set]set main layout, init view

--- app/src/main/java/com/example/imagedecoder_outsourcing/MainActivity.java
+++ app/src/main/java/com/example/imagedecoder_outsourcing/MainActivity.java
@@ -3,13 +3,27 @@ package com.example.imagedecoder_outsourcing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton cameraButton; //카메라버튼
+    private ImageButton galleryButton; //갤러리버튼
+    private ImageButton fileButton; //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        initView();
+
+    }
+
+    private void initView() {
+        cameraButton = (ImageButton)findViewById(R.id.cameraButton);
+        galleryButton = (ImageButton)findViewById(R.id.galleryButton);
+        fileButton = (ImageButton)findViewById(R.id.fileButon);
     }
 }
